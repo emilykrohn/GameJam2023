2b82486ca446c69e491766b58ebd2e41bc04ee86
Alyssa Barrientos <aly3barr@gmail.com>
2023-09-30 14:15:37 -0700

    Player addon
    I made the player scene and I am currently adding movement

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+# How fast the player moves in meters per second.
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO 
+#3D vector combining a speed with a direction. Here, we define it as a property because we want to update and reuse its value across frames.
+
+func _physics_process(delta):
+	# We create a local variable to store the input direction.
+	var direction = Vector3.ZERO
+
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
