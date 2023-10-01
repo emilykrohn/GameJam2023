ed214de91889288398aa81537f720d43a4a22abb
Emily Krohn <emilykrohn2@gmail.com>
2023-10-01 15:19:17 -0700

    Make enemies follow the player

--- mob.gd
+++ mob.gd
@@ -1,13 +1,16 @@
 extends CharacterBody3D
 
 @export var enemy_speed = 5
+var current_player_position
 
 func _physics_process(_delta):
+	current_player_position = Global.player_position
+	velocity = (current_player_position - position).normalized() * enemy_speed
 	move_and_slide()
+	look_at(current_player_position)
 
 func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
-	rotate_y(randf_range(-PI / 4, PI / 4))
 
 	velocity = (player_position - start_position).normalized() * enemy_speed
 
