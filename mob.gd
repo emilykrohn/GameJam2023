c5cbe88f6851eb9593433c5df25ccd229f1d740c
Emily Krohn <emilykrohn2@gmail.com>
2023-09-30 18:07:54 -0700

    Enemy Moves Toward Player

--- mob.gd
+++ mob.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-@export var enemy_speed = 15
+@export var enemy_speed = 5
 
 func _physics_process(_delta):
 	move_and_slide()
@@ -9,6 +9,5 @@ func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	rotate_y(randf_range(-PI / 4, PI / 4))
 
-	velocity = Vector3.FORWARD * enemy_speed
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	velocity = (player_position - start_position).normalized() * enemy_speed
 
